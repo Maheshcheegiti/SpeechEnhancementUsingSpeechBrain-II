4dff6497fb914f3d84f6f0d8e5369e4de842c834
Cheegiti Mahesh <ramahesh024@gmail.com>
2023-04-29 00:29:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,41 @@
 import streamlit as st
 from speechbrain.pretrained import SepformerSeparation as separator
 import torchaudio
-import os
 
-# Download the model and set the save directory
-model = separator.from_hparams(source="speechbrain/sepformer-whamr-enhancement", savedir='pretrained_models/sepformer-whamr-enhancement')
 
-# Define a function to process the uploaded file
-def process_file(uploaded_file):
-    # Save the uploaded file to the disk
-    with open(os.path.join("uploads", uploaded_file.name), "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    file_path = os.path.join("uploads", uploaded_file.name)
+def run_audio_enhancement():
+    # Load the model
+    model = separator.from_hparams(source="speechbrain/sepformer-whamr-enhancement", savedir='pretrained_models/sepformer-whamr-enhancement')
 
-    # Separate the audio sources using the model
-    est_sources = model.separate_file(path=file_path)
+    # Create a file uploader
+    uploaded_file = st.file_uploader("Upload an audio file", type=["wav", "mp3"])
 
-    # Save the enhanced audio to the disk
-    torchaudio.save(os.path.join("uploads", "enhanced_" + uploaded_file.name), est_sources[:, :, 0].detach().cpu(), 8000)
-
-    return file_path, os.path.join("uploads", "enhanced_" + uploaded_file.name)
-
-# Define the main function
-def main():
-    st.title("Speech Enhancement using SpeechBrain")
-    st.write("Upload a noisy audio file to enhance it.")
-
-    # Create an uploader widget
-    uploaded_file = st.file_uploader("Choose a file")
-
-    # If a file is uploaded
+    # Check if a file has been uploaded
     if uploaded_file is not None:
-        # Process the uploaded file
-        file_path, enhanced_file_path = process_file(uploaded_file)
+        # Load the audio file
+        audio_data = uploaded_file.read()
+        audio_tensor, sample_rate = torchaudio.load(audio_data)
 
-        # Show the original audio
-        st.audio(file_path, format="audio/wav", caption="Original Audio")
+        # Apply the SepFormer model to the audio file
+        est_sources = model.separate_tensor(audio_tensor)
 
-        # Show the enhanced audio
-        st.audio(enhanced_file_path, format="audio/wav", caption="Enhanced Audio")
+        # Save the enhanced audio file
+        torchaudio.save("enhanced_audio.wav", est_sources[:, :, 0].detach().cpu(), sample_rate)
 
-# Run the app
+        # Display the original and enhanced audio files
+        st.audio(audio_data, format='audio/wav')
+        st.audio("enhanced_audio.wav", format='audio/wav')
+
+
+def main():
+    # Set the title and description
+    st.set_page_config(page_title="Audio Enhancement with SepFormer")
+    st.title("Audio Enhancement with SepFormer")
+    st.write("This app uses the SepFormer model to enhance audio.")
+
+    # Run the audio enhancement function
+    run_audio_enhancement()
+
+
 if __name__ == "__main__":
     main()
