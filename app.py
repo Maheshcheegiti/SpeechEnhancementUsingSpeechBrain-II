0e2b15c58a7ac8d164516c13485477c85446636b
Cheegiti Mahesh <ramahesh024@gmail.com>
2023-04-29 00:22:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,35 @@
-import streamlit as st
-import torchaudio
 from speechbrain.pretrained import SepformerSeparation as separator
+import torchaudio
+import streamlit as st
+import os
+
+ALLOWED_EXTENSIONS = {'wav'}
 
 model = separator.from_hparams(source="speechbrain/sepformer-whamr-enhancement", savedir='pretrained_models/sepformer-whamr-enhancement')
 
-def process_file(file_path):
-    waveform, sample_rate = torchaudio.load(file_path)
-    est_sources = model.separate_file(path=file_path)
-    return waveform, est_sources[:, :, 0], sample_rate
-
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() == 'wav'
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def process_file(file):
+    est_sources = model.separate_file(path=file) 
+    return est_sources[:, :, 0].detach().cpu()
 
 def main():
     st.set_page_config(page_title="Speech Enhancement", page_icon="🔊", layout="wide")
+    st.title("Speech Enhancement")
 
-    st.title("Speech Enhancement - SpeechBrain - SepFormer")
-
-    uploaded_file = st.file_uploader("Upload an audio file", type=["wav"])
+    uploaded_file = st.file_uploader("Upload an audio file", type=ALLOWED_EXTENSIONS)
 
     if uploaded_file is not None:
         if allowed_file(uploaded_file.name):
-        with st.spinner("Processing..."):
-            file_path = os.path.join("uploads", uploaded_file.name)
-            with open(file_path, "wb") as f:
-                f.write(uploaded_file.getbuffer())
-            speech, enhanced, sr = process_file(file_path)
-        st.audio(speech, format='audio/wav', start_time=0, caption="Original Audio",  sample_rate=sr)
-        st.audio(enhanced, format='audio/wav', start_time=0, caption="Enhanced Audio", sample_rate=sr)
-    else:
-        st.warning("Invalid file type. Please upload a WAV file.")
+            with st.spinner("Processing..."):
+                enhanced = process_file(uploaded_file)
+            speech, sr = torchaudio.load(uploaded_file)
+            st.audio(speech, format='audio/wav', start_time=0, caption="Original Audio",  sample_rate=sr)
+            st.audio(enhanced, format='audio/wav', start_time=0, caption="Enhanced Audio",  sample_rate=8000)
+        else:
+            st.warning("Invalid file type. Please upload a WAV file.")
+
 if __name__ == '__main__':
     main()
